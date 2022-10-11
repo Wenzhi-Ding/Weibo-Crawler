4d352e2ae4805adfaaebfbcad0aafb8990407996
Wenzhi Ding <wenzhi.ding@foxmail.com>
2022-10-11 13:13:05 +0800

    Update update_code.py

--- tool/update_code.py
+++ tool/update_code.py
@@ -1,6 +1,8 @@
 import os
 
 print('尝试更新代码')
+os.system('git add --all')
+os.system('git commit -m "update"')
 os.system('git reset --hard origin/master')
 os.system('git pull origin master')
 print('已完成更新')
